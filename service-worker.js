eddd3dd4572019045a9e00a28ccfda679145a222
gcm-visa <visa@gcm.edu.ph>
2025-04-09 22:11:36 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
 const CACHE_NAME = 'gcm-visa-cache-v1';
 const URLs_TO_CACHE = [
-  '/gcm-visa-app/',
-  '/gcm-visa-app/index.html',
-  '/gcm-visa-app/script.js',
-  '/gcm-visa-app/icon-192x192.png',
-  '/gcm-visa-app/icon-512x512.png'
+  '/gcm-visa-app-admin/',
+  '/gcm-visa-app-admin/index.html',
+  '/gcm-visa-app-admin/script.js',
+  '/gcm-visa-app-admin/icon-192x192.png',
+  '/gcm-visa-app-admin/icon-512x512.png'
 ];
 
 self.addEventListener('install', (event) => {
